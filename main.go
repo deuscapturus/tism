63ebffdec3426ec9e045e60c000c86209a18eba7
Theodore Cowan <theodore-cowan@pluralsight.com>
2016-09-21 06:41:24 -0600

    added gpgkeys, is able to decrypt messages

--- main.go
+++ main.go
@@ -5,6 +5,11 @@ import (
 	"net/http"
 	"log"
 	"encoding/json"
+	"os"
+	"encoding/base64"
+	"bytes"
+	"golang.org/x/crypto/openpgp"
+	"io/ioutil"
 )
 
 
@@ -12,12 +17,51 @@ import (
 // hello world, the web server
 
 func DecryptSecret(s string) string {
-	return s
+
+        //all taken from here https://gist.github.com/stuart-warren/93750a142d3de4e8fdd2
+	//TODO: Figure out how i should properly close this file.
+        keyringFileBuffer, err := os.Open("./gpgkeys/secring.gpg")
+	if err != nil {
+		log.Println(err)
+		return ""
+	}
+	entityList, err := openpgp.ReadKeyRing(keyringFileBuffer)
+	if err != nil {
+		log.Println(err)
+		return ""
+	}
+	//entity := entityList[0]
+        dec, err := base64.StdEncoding.DecodeString(s)
+	if err != nil {
+		log.Println(err)
+		return ""
+	}
+        md, err := openpgp.ReadMessage(bytes.NewBuffer(dec), entityList, nil, nil)
+	if err != nil {
+		log.Println(err)
+		return ""
+	}
+	bytes, err := ioutil.ReadAll(md.UnverifiedBody)
+	if err != nil {
+		log.Println(err)
+		return ""
+	}
+	decStr := string(bytes)
+
+        return decStr
+
 }
+
 
 func HandleRequest(w http.ResponseWriter, req *http.Request) {
 	//Grab the contents of the request.  Make sure we have a valid object.  If not return error
-	// curl -H "Content-Type: application/json" -X POST -d '{"Key":"xyz","GpgContents":"xyz"}' http://localhost:8080/
+	// Encrypt a message
+	// =================
+	// echo -n "this was encrypted" | gpg --batch --trust-model always --encrypt -r "Test Accounting <test@fakedomain.com>" --homedir ./gpgkeys/ | base64 -w 0
+
+	// Decrypt that message
+	// ====================
+	// curl -H "Content-Type: application/json" -X POST -d '{"Key":"xyz","GpgContents":"hQEMA5siECpJxYMfAQf8D3+/slhHH47advUt+J8UZaQhP+fXiwR/R/GHalti3mFg6GknMIr7lKbxitGMsBFkTn+Qt1Mp0EuM+Z/suG1Jl+ppZ3WD5KjoBjZbDB8JYQw9aqPhQ/gl/M0GMwrZeyIUbHhtTUaEGZz53lcVWec6HItyGhEW3Lhv9MwciX4CvYp6aj3bJ6XLLfvTZe0qFBibMx8/VYX2gaJLZTC4kkTs7JXDVSzjGRKITnlVCUt+Qb8t8NkC/MzDH5xHZEts+KOy7TF5AQ7LBJ5mJL3X0WJtnnA7MSFFHABiT5/0Oa+SsH2aCv3AOb8l7FipVLxC3oJEbdEyp8eMMr54+pWDRRoreNJNAZMa2Fyt4wsDEbm6gsQzjjJ7e2Lj8osnDfiLalIEgczYHkkMpUaqNWlb+ErEUewKuMYahNg49op5RGu+8J+S+8cEJb2cWNdlLoi3kME="}"}' http://localhost:8080/
 	json := json.NewDecoder(req.Body)
 
 	type Request struct {
@@ -25,7 +69,7 @@ func HandleRequest(w http.ResponseWriter, req *http.Request) {
 		GpgContents string
 	}
 
-	//TODO: if json fails return error with code 400
+	//TODO: Validate that the json keys Key and GpgConents exist in the json else return 400.
 	var r Request
 	err := json.Decode(&r)
 	if err != nil {
@@ -39,7 +83,14 @@ func HandleRequest(w http.ResponseWriter, req *http.Request) {
 
 	//if r.Key is a valid jwt decrypt.
 	if r.Key == "xyz" {
-		io.WriteString(w, DecryptSecret(r.Key))
+		w.Header().Set("Content-Type", "text/plain")
+		decrypted := DecryptSecret(r.GpgContents)
+		if decrypted == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			io.WriteString(w, "Invalid Request")
+		} else {
+		io.WriteString(w, decrypted)
+		}
 
 	} else {
 		w.WriteHeader(http.StatusUnauthorized)
