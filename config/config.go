32008147919df6cd1abc68f6eb9542c2e502d9d0
Theodore Cowan <theodore-cowan@pluralsight.com>
2016-10-16 11:52:28 -0600

    Add the feature to generate JWT token secrets on first startup

--- config/config.go
+++ config/config.go
@@ -3,9 +3,12 @@ package config
 
 import (
 	"flag"
+	"github.com/deuscapturus/tism/randid"
 	"github.com/go-yaml/yaml"
 	"io/ioutil"
 	"log"
+	"os"
+	"text/template"
 )
 
 type Configuration struct {
@@ -19,29 +22,29 @@ type Configuration struct {
 	KeyRingFilePath string   `yaml:"keyring_path,omitempty"`
 	GenAdminToken   bool
 	GenCert         bool
+	ConfigPath      string
 }
 
-var Config *Configuration
+// Config default configuration values
+var Config = &Configuration{
+	KeyRingFilePath: "gpgkeys/secring.gpg",
+	TLSCertFile:     "cert.crt",
+	TLSKeyFile:      "cert.key",
+	TLSDir:          "./cert/",
+}
 
 // Load configuration at startup
 func init() {
+	parseFlags()
+	renderConfigTemplate()
 	Load()
 }
 
-// LoadConfiguration load athe configuration from the provided confioguration path.
-func Load() {
+// ParseFlags Command line configuration values
+func parseFlags() {
 
-	// Default configuration values
-	Config = &Configuration{
-		KeyRingFilePath: "gpgkeys/secring.gpg",
-		TLSCertFile:     "cert.crt",
-		TLSKeyFile:      "cert.key",
-		TLSDir:          "./cert/",
-	}
 
-	// Command line configuration values
 	ConfigPath := flag.String("config", "config.yaml", "Configuration file path.")
-
 	GenAdminToken := flag.Bool("t", false, "Generate a super admin token")
 	GenCert := flag.Bool("c", false, "Generate a new TLS certificate. WARNING; WILL OVERWRITE")
 	Port := flag.String("p", "8080", "Port to listen on")
@@ -49,7 +52,41 @@ func Load() {
 
 	flag.Parse()
 
-	ConfigFileBytes, err := ioutil.ReadFile(*ConfigPath)
+	Config.GenAdminToken = *GenAdminToken
+	Config.GenCert = *GenCert
+	Config.ConfigPath = *ConfigPath
+	Config.Port = *Port
+	Config.Address = *Address
+}
+
+// renderConfigTemplate
+func renderConfigTemplate() {
+
+	configTemplate, err := template.ParseFiles(Config.ConfigPath)
+	if err != nil {
+		log.Println(err)
+	}
+
+	type ConfigValues struct {
+		TokenSecret string
+	}
+	configTemplate.Option("missingkey=error")
+	//TODO Do not touch the file unless template variables are found.
+	configFile, err := os.Create(Config.ConfigPath)
+	if err != nil {
+		log.Fatal("Unable to create configuration file")
+	}
+
+	err = configTemplate.Execute(configFile, ConfigValues{generateTokenSecret()})
+	if err != nil {
+		log.Fatal("Unable to write configuration file")
+	}
+}
+
+// Load configuration from the provided confioguration path.
+func Load() {
+
+	ConfigFileBytes, err := ioutil.ReadFile(Config.ConfigPath)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -58,8 +95,13 @@ func Load() {
 		log.Fatal(err)
 	}
 
-	Config.GenAdminToken = *GenAdminToken
-	Config.GenCert = *GenCert
-	Config.Port = *Port
-	Config.Address = *Address
+	if Config.JWTsecret == "" {
+		log.Fatal("TokenSecret is undefined")
+	}
+}
+
+// generateTokenSecret generates a random 256 character string
+func generateTokenSecret() string {
+
+	return randid.GenerateSecret(256)
 }
