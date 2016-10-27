01d5d86efb1c350405a1f843d1c1c77f9ef0026f
Theodore Cowan <theodore-cowan@pluralsight.com>
2016-10-26 21:21:41 -0600

    mostly working. token does not update keys

--- client/app.js
+++ client/app.js
@@ -25,22 +25,27 @@ tISM.controller = function() {
 	// when this controller is updated perform a new POST for data by calling message?
 	var token = m.prop("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbiI6MSwiZXhwIjo5OTk5OTk5OTk5OSwianRpIjoiNGI2dmk2aTV0NWphZCIsImtleXMiOlsiQUxMIl19.zi5Ei-ZJcl-8dUVW27vGZLIM6qOFfwk94r_aauqr-tQ")
 
-	var keys = function() {
-		tISM.keys(this.token)
-	}
-	return { token, keys }
+	var keys = new tISM.keys(token)
+
+	return { 
+		token,
+		keys
+       	}
 };
 
 //view
 tISM.view = function(ctrl) {
 	return m("div"), [
+		m("p", ctrl.token()),
 		m("ol", ctrl.keys().map( function(key, index) {
 			return m("li", key.Id, key.Name, key.CreationTime)
 		})),
 		m("input", {
-			onchange: m.withAttr("value", ctrl.token)
+			oninput: m.withAttr("value", ctrl.token),
+			value: ctrl.token()
 		})
 	]
+
 };
 
 
