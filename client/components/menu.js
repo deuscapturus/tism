f590d0ef26f14454edb7c2592c31efe388a5d130
Theodore Cowan <theodore-cowan@pluralsight.com>
2017-04-19 06:31:39 -0700

    update client ui to boostrap beta 4

--- client/components/menu.js
+++ client/components/menu.js
@@ -4,10 +4,10 @@ import { Token } from "../models/Token.js"
 var menu = {
 	view: function() {
 		return m("div", m("ul[class=nav nav-tabs]", [
-			m("li", {class: (m.route.param("task") == "decrypt") ? "active" : null}, m("a[href=/decrypt]", {oncreate: m.route.link}, "Decrypt")),
-			m("li", {class: (m.route.param("task") == "encrypt") ? "active" : null}, m("a[href=/encrypt]", {oncreate: m.route.link}, "Encrypt")),
-			m("li", {class: (m.route.param("task") == "keys") ? "active" : null}, m("a[href=/keys]", {oncreate: m.route.link}, "Keys")),
-			m("li", {class: (m.route.param("task") == "tokens") ? "active" : null}, m("a[href=/tokens]", {oncreate: m.route.link}, "Tokens")),
+			m("li[class=nav-item]", m("a[href=/decrypt][class=nav-link]", {oncreate: m.route.link, class: (m.route.param("task") == "decrypt") ? "active" : null}, "Decrypt")),
+			m("li[class=nav-item]", m("a[href=/encrypt][class=nav-link]", {oncreate: m.route.link, class: (m.route.param("task") == "encrypt") ? "active" : null}, "Encrypt")),
+			m("li[class=nav-item]", m("a[href=/keys][class=nav-link]", {oncreate: m.route.link, class: (m.route.param("task") == "keys") ? "active" : null}, "Keys")),
+			m("li[class=nav-item]", m("a[href=/tokens][class=nav-link]", {oncreate: m.route.link, class: (m.route.param("task") == "tokens") ? "active" : null}, "Tokens")),
 	//		(Token.info.admin == 1) ? m("li[class=dropdown]",
 	//			{class: (m.route.param("task") == "tokens"|| m.route.param("task") == "keys") ? "active" : null}, [
 	//			m("a[href=#][class=dropdown-toggle][data-toggle=dropdown][role=button][aria-haspopup=true][aria-expanded=false]",
--- client/components/tokens.js
+++ client/components/tokens.js
@@ -9,7 +9,6 @@ var tokens = {
 	view: function(vnode) {
 		return m("div", [
 			m("button[type=button][class=btn btn-default]", { onclick: function() { CreateToken.create() }}, m("span[class=glyphicon glyphicon-plus][aria-hidden=true]", " Create Token")),
-			m("div[id=tokenModal]"),
 			m("table[class=table table-striped table-hover]", [
 				m("thead", [
 					m("tr", [
@@ -23,7 +22,7 @@ var tokens = {
 						return m("tr", {onclick: function() {
 							CreateToken.toggle(key.Id)
 							console.log(CreateToken.selectedKeys)
-						}, class: (CreateToken.selectedKeys.some(x => x === key.Id)) ? "active" : null
+						}, class: (CreateToken.selectedKeys.some(x => x === key.Id)) ? "table-active" : null
 						},[
 							m("th[scope=row]", key.Id),
 							m("td", key.Name),
