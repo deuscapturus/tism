906a007d37b1e950dc1ef4b3eba3883a3857f279
Theodore Cowan <theodore-cowan@pluralsight.com>
2017-04-24 14:50:01 -0600

    fix client to respect newlines in modal

--- client/components/modal.js
+++ client/components/modal.js
@@ -11,7 +11,7 @@ var modal = {
 		return m("div[class=modal-dialog modal-lg]", m("div[class=modal-content]", [
 			m("div[class=modal-header]", m("h5[class=modal-title]", "New Token" )), 
 			m("div[class=modal-body]", [
-				m("p[style=word-wrap: break-word;]", vnode.attrs.message),
+				m("p[style=word-wrap: break-word; white-space: pre-wrap;]", vnode.attrs.message),
 				m("button[class=close]", { onclick: function() {
 					m.render(vnode.dom.parentNode, null)
 				}}, "Close")]
