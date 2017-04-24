fa49192ea77166d90dd4010de4a50073cdd01bda
Theodore Cowan <theodore-cowan@pluralsight.com>
2017-04-24 11:39:34 -0600

    client session handling for token information

--- client/models/Token.js
+++ client/models/Token.js
@@ -5,8 +5,8 @@ var Token = {
 	current: sessionStorage.getItem("token"),
 	error: sessionStorage.getItem("tokenError"),
 	info: JSON.parse(sessionStorage.getItem("tokenInfo")) || {},
-	adminString: "",
-	numKeys: 0,
+	adminString: sessionStorage.getItem("adminString") || "",
+	numKeys: sessionStorage.getItem("numKeys") || 0,
 	getInfo: function(token) {
 		return m.request({
 			method: "POST",
@@ -17,7 +17,9 @@ var Token = {
 			Token.info = result
 			sessionStorage.setItem("tokenInfo", JSON.stringify(result))
 			Token.adminString = (result.admin == 1) ? "Admin" : "Non-Admin"
+			sessionStorage.setItem("adminString", Token.adminString)
 			Token.numKeys = (Token.info.keys[0] == "ALL") ? "ALL" : Token.info.keys.length
+			sessionStorage.setItem("numKeys", Token.numKeys)
 			Keys.getList()
 		})
 		.catch(function(e) {
