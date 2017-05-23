8411921ffec0f323d27b05b818a63b814e3d2fef
Theodore Cowan <theodore-cowan@pluralsight.com>
2017-05-22 22:32:46 -0600

    add benchmark test for request

--- request/request_test.go
+++ request/request_test.go
@@ -13,7 +13,7 @@ func TestParse(t *testing.T) {
 
 	// Variables stub/mock
 	token := "12345.abcde.98765"
-	data := `{"token": "` + token + `"}"`
+	data := `{"token": "` + token + `"}`
 
 	// Create a stub/mock request with http.NewRequest
 	req, err := http.NewRequest("POST", "/", bytes.NewBufferString(data))
@@ -45,4 +45,41 @@ func TestParse(t *testing.T) {
 	})
 
 	handler.ServeHTTP(res, req)
+}
+
+// BenchmarkParse performance check for Parse.
+func BenchmarkParse(b *testing.B) {
+
+	// Variables stub/mock
+	token := "12345.abcde.98765"
+	data := `{"token": "` + token + `"}`
+
+
+	// Create a test response recorder
+	res := httptest.NewRecorder()
+
+	// Create http handler.
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		err, _ := Parse(w, *r)
+		if err != nil {
+			b.Fatal(err)
+		}
+		//r.Body.Close()
+
+	})
+
+	b.ResetTimer()
+	// Test Parse function for errors
+	for i := 0; i < b.N; i++ {
+		// Create a stub/mock request with http.NewRequest. The req Body io.ReadCloser
+		// is closed with every request.  So it must be in the loop to be recreated.
+		req, err := http.NewRequest("POST", "/", bytes.NewBufferString(data))
+		if err != nil {
+			b.Fatal(err)
+		}
+
+		handler.ServeHTTP(res, req)
+	}
+
 }
